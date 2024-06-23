ed4a19454eb69ce5d9e2a3a1536cd16c8bae30f2
Yonathan Levi <jonilevi@icloud.com>
2024-06-23 16:37:21 -0400

    ab

--- src/app/api/update-cart/route.js
+++ src/app/api/update-cart/route.js
@@ -3,33 +3,32 @@ import { NextResponse } from "next/server";
 import { redirect } from "next/navigation";
 export async function POST(req) {
   try {
-    // const body = await req.json();
-    // const { id, name, email, orderMethod } = body;
+    const body = await req.json();
+    const { id, name, email, orderMethod } = body;
 
-    // if (!name || !email || !id || !orderMethod) {
-    //   return NextResponse.json(
-    //     { message: "Missing required fields" },
-    //     { status: 400 }
-    //   );
-    // }
-    // const cart = await swell.put("/carts/{id}", {
-    //   id: id,
-    //   shipping: {
-    //     pickup: orderMethod,
-    //     country: "CA",
-    //     state: "QC",
-    //     city: "Montreal",
-    //   },
-    //   billing: {
-    //     name: name,
-    //   },
-    //   account: {
-    //     email: email,
-    //   },
-    // });
-    // console.log("Updated cart:", cart);
-    const data = await req.json();
-    console.log("the data is:", data);
+    if (!name || !email || !id || !orderMethod) {
+      return NextResponse.json(
+        { message: "Missing required fields" },
+        { status: 400 }
+      );
+    }
+    const cart = await swell.put("/carts/{id}", {
+      id: id,
+      shipping: {
+        pickup: orderMethod,
+        country: "CA",
+        state: "QC",
+        city: "Montreal",
+      },
+      billing: {
+        name: name,
+      },
+      account: {
+        email: email,
+      },
+    });
+    console.log("Updated cart:", cart);
+
     return NextResponse.json(data);
     // return NextResponse.redirect(process.env.NEXT_PUBLIC_HOME_URL + "/payment");
   } catch (error) {
