3b9b2bde08dbd4b8f28c42293a8b3f72cefa0f0b
Yonathan Levi <jonilevi@icloud.com>
2024-06-23 18:54:18 -0400

    test api

--- src/app/api/test/route.js
+++ src/app/api/test/route.js
@@ -1,5 +1,6 @@
 import { NextResponse } from "next/server";
 import { swell } from "@/lib/swell/node";
+
 export async function GET() {
   const cart = await swell.get("/carts");
   console.log(cart, "get cart test");
