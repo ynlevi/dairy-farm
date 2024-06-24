c6b36f05707a27f8c231c1ca351048702b3cef01
Yonathan Levi <jonilevi@icloud.com>
2024-06-23 21:55:39 -0400

    lets try this out

--- src/app/payment/apple-pay/page.jsx
+++ src/app/payment/apple-pay/page.jsx
@@ -1,42 +1,32 @@
-"use client";
 import React, { useEffect } from "react";
 import { swell } from "@/lib/swell/client";
-export default function page() {
-  useEffect(() => {
-    const initializePayment = async () => {
-      await swell.payment.createElements({
-        apple: {
-          //   style: {
-          // theme: "<button-theme>", // Default: black
-          // type: "<button-type>", // Default: plain
-          // height: "<button-height>", // Default: 40px
-          //   },
-          require: {
-            // Requested data in Apple Pay modal
-            shipping: false, // Default: false
-            name: true, // Default: false
-            email: true, // Default: false
-            phone: false, // Default: false
-          },
-          classes: {
-            base: "<button-container-class>", // Optional, the base class applied to the container
-            // The following classes only work with Stripe Apple Pay
-            complete: "<button-complete-class>", // Optional, the class name to apply when the Element is complete
-            empty: "<button-empty-class>", // Optional, the class name to apply when the Element is empty
-            focus: "<button-focus-class>", // Optional, the class name to apply when the Element is focused
-            invalid: "<button-invalid-class>", // Optional, the class name to apply when the Element is invalid
-            webkitAutofill: "<button-webkit-autofill-class>", // Optional, the class name to apply when the Element has its value autofilled by the browser (only on Chrome and Safari)
-          },
-          onSuccess: () => {}, // Optional, called on submit Apple Pay modal
-          onError: (error) => {}, // Optional, called on payment error
-        },
-      });
-    };
+export default async function page() {
+  await swell.payment.createElements({
+    apple: {
+      require: {
+        // Requested data in Apple Pay modal
+        shipping: false, // Default: false
+        name: true, // Default: false
+        email: true, // Default: false
+        phone: false, // Default: false
+      },
+      classes: {
+        base: "<button-container-class>", // Optional, the base class applied to the container
+        // The following classes only work with Stripe Apple Pay
+        complete: "<button-complete-class>", // Optional, the class name to apply when the Element is complete
+        empty: "<button-empty-class>", // Optional, the class name to apply when the Element is empty
+        focus: "<button-focus-class>", // Optional, the class name to apply when the Element is focused
+        invalid: "<button-invalid-class>", // Optional, the class name to apply when the Element is invalid
+        webkitAutofill: "<button-webkit-autofill-class>", // Optional, the class name to apply when the Element has its value autofilled by the browser (only on Chrome and Safari)
+      },
+      onSuccess: () => {}, // Optional, called on submit Apple Pay modal
+      onError: (error) => {}, // Optional, called on payment error
+    },
+  });
+  initializePayment();
 
-    initializePayment();
-  }, []);
   return (
-    <div className=" bg-yellow-500   p-4 ">
+    <div className="   p-4 ">
       <div id="applepay-button"></div>
       {/* <button onClick={submitPayment}>Pay Now</button> */}
     </div>
