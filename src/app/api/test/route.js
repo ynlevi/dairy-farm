05ffa881c0197927b4d6c3194b152a84922ca9dd
Yonathan Levi <jonilevi@icloud.com>
2024-06-23 20:02:25 -0400

    bas

--- src/app/api/test/route.js
+++ src/app/api/test/route.js
@@ -2,9 +2,9 @@ import { NextResponse } from "next/server";
 import { swell } from "@/lib/swell/node";
 
 export async function GET() {
-  // const cart = await swell.get("/carts");
-  const rendomApi = await fetch("https://jsonplaceholder.typicode.com/todos/1");
-  const cart = await rendomApi.json();
+  const cart = await swell.get("/carts");
+  // const rendomApi = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+
   console.log(cart, "get cart test");
   return NextResponse.json({ cart: cart });
 }
